64160b64c81fb2839ad076e7b97db34c62e06b16
Morgan-Saturn <morgan.tunteva@proton.me>
2026-07-07 10:36:43 +0200

    created the xml parser file

---
+++ recuperation_data_rss.php
@@ -0,0 +1,8 @@
+<?php
+    $alocine_rss = "https://www.allocine.fr/rss/news-cine.xml";
+    $rss_load = simplexml_load_file($alocine_rss);
+    foreach ($rss_load->channel->item as $item) {
+        echo $item->title . PHP_EOL;
+    }
+
+?>
