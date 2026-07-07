e2ccc82ff20a7d56b5e58e08e7c627667b3daef0
Morgan-Saturn <morgan.tunteva@proton.me>
2026-07-07 10:44:42 +0200

    recreated the branch i accidentally deleted

--- recuperation_data_rss.php
+++ recuperation_data_rss.php
@@ -2,7 +2,7 @@
     $alocine_rss = "https://www.allocine.fr/rss/news-cine.xml";
     $rss_load = simplexml_load_file($alocine_rss);
     foreach ($rss_load->channel->item as $item) {
-        echo $item->title . PHP_EOL;
+        echo $item->title."<br>";
     }
 
 ?>
