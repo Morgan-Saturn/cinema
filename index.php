90fffe70399ad962717effd0d2f470659e978c4f
Morgan-Saturn <morgan.tunteva@proton.me>
2026-07-07 14:15:43 +0200

    moved the scrapper to the index.html page, which I modified to .php so it can accept php

--- index.php
+++ index.php
@@ -1,10 +1,43 @@
+<?php
+    libxml_use_internal_errors(true); //permet de gérer les erreurs sans crasher tout le script php
+
+    //mise en cache du flux rss
+    if(time() - filemtime("cache_allo_cine.xml") > 1800) //1800 correspond à 30 minutes converties en secondes
+        {
+        $allocine_rss = "https://www.allocine.fr/rss/news-cine.xml";
+        //si le serveur d'allocine est inaccessible
+        if (!empty($allocine_rss)) 
+            {
+            file_put_contents("cache_allo_cine.xml", $allocine_rss);
+            }
+        else    
+            {
+                $allocine_rss = file_get_contents("cache_allo_cine.xml");
+            }
+        }
+        else
+        {
+            $allocine_rss = file_get_contents("cache_allo_cine.xml");
+        } 
+
+        $rss_load = simplexml_load_file($allocine_rss);
+        foreach ($rss_load->channel->item as $item) {
+            $title = htmlspecialchars($item->title ?? 'No title');
+            $link = htmlspecialchars($item->link ?? '#');
+            $description = htmlspecialchars($item->description ?? 'No description');
+            echo "<li><a href=\"$link\">$title</a>$description</li>";
+            //echo $item->link."<br>";
+        }
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="/style.css" rel="stylesheet"/>
-    <link rel="icon" type="image/png" sies="32x32" href="/assets/cinema.png"/>
+    <link href="style.css" rel="stylesheet"/>
+    <link rel="icon" type="image/png" sies="32x32" href="assets/cinema.png"/>
 
     <title>Films à l'affiche</title>
 </head>
@@ -29,32 +62,32 @@
             <h2>FILMS A L'AFFICHE</h2>
             <div class="movie_grid">
                 <div class="movie_1">
-                    <img class="movie_img" src="/assets/example_movie.png" alt="affiche du film">
+                    <img class="movie_img" src="assets/example_movie.png" alt="affiche du film">
                     <h3 class="categories">Film1</h3>
                     <p class="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. </p>
                 </div>
                 <div class="movie_2">
-                    <img class="movie_img" src="/assets/example_movie.png" alt="affiche du film">
+                    <img class="movie_img" src="assets/example_movie.png" alt="affiche du film">
                     <h3 class="categories">Film2</h3>
                     <p class="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. </p>
                 </div>
                 <div class="movie_3">
-                    <img class="movie_img" src="/assets/example_movie.png" alt="affiche du film">
+                    <img class="movie_img" src="assets/example_movie.png" alt="affiche du film">
                     <h3 class="categories">Film3</h3>
                     <p class="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. </p>
                 </div>
                 <div class="movie_4">
-                    <img class="movie_img" src="/assets/example_movie.png" alt="affiche du film">
+                    <img class="movie_img" src="assets/example_movie.png" alt="affiche du film">
                     <h3 class="categories">Film4</h3>
                     <p class="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. </p>
                 </div>
                 <div class="movie_5">
-                    <img class="movie_img" src="/assets/example_movie.png" alt="affiche du film">
+                    <img class="movie_img" src="assets/example_movie.png" alt="affiche du film">
                     <h3 class="categories">Film5</h3>
                     <p class="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. </p>
                 </div>
                 <div class="movie_6">
-                    <img class="movie_img" src="/assets/example_movie.png" alt="affiche du film">
+                    <img class="movie_img" src="assets/example_movie.png" alt="affiche du film">
                     <h3 class="categories">Film6</h3>
                     <p class="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. </p>
                 </div>
