a7c7880149aeee4fea848116b6b21292daf2e5e6
Morgan-Saturn <morgan.tunteva@proton.me>
2026-07-07 11:28:52 +0200

    add keeping in memory of allocine's rss feed xml

--- recuperation_data_rss.php
+++ recuperation_data_rss.php
@@ -1,8 +1,27 @@
 <?php
-    $alocine_rss = "https://www.allocine.fr/rss/news-cine.xml";
-    $rss_load = simplexml_load_file($alocine_rss);
-    foreach ($rss_load->channel->item as $item) {
-        echo $item->title."<br>";
-    }
+    //mise en cache du flux rss
+    if(time() - filemtime("cache_allo_cine.xml") > 1800) //1800 correspond à 30 minutes converties en secondes
+        {
+        $allocine_rss = "https://www.allocine.fr/rss/news-cine.xml";
+        //si le serveur d'allocine est inaccessible
+        if (!empty($allocine_rss)) 
+            {
+            file_put_contents("cache_allo_cine.xml", $allocine_rss);
+            }
+        else    
+            {
+                $allocine_rss = file_get_contents("cache_allo_cine.xml");
+            }
+        }
+        else
+        {
+            echo "cache cache : <br/>";
+            $allocine_rss = file_get_contents("cache_allo_cine.xml");
+        } 
+
+        $rss_load = simplexml_load_file($allocine_rss);
+        foreach ($rss_load->channel->item as $item) {
+            echo $item->title."<br>";
+        }
 
 ?>
