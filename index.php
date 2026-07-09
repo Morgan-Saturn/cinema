9dfb73d68ec34ca301da5053a3f8bc3744b60f11
Morgan-Saturn <morgan.tunteva@proton.me>
2026-07-09 09:23:07 +0200

    changed a variable's name so it fits more and makes what it's doing more clear, and changed a little the if part so i don't have the same instruction twice

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php
-    //libxml_use_internal_errors(true); //permet de gérer les erreurs sans crasher tout le script php
+    libxml_use_internal_errors(true); //permet de gérer les erreurs sans crasher tout le script php
 
     //cURL sert à faire des appels d'API, fetch des pages web ou envoyer des données à des serveurs avec des url. Il permet de faire des requêtes http.
 
     $allocine_rss = "https://www.allocine.fr/rss/news-cine.xml";
     $cache_allocine = "cache_allocine.xml";
+    $contenu = NULL;
 
     if (!file_exists($cache_allocine) || time() - filemtime($cache_allocine) > 1800) //1800s = 30 min
     {
-        $url = curl_init($allocine_rss);
-        curl_setopt($url, CURLOPT_RETURNTRANSFER, true); //curlopt_returntransfer retourne le fichier sous forme de chaînes de caractères
-        $contenu = curl_exec($url) or die(curl_error($url));
+        $rss_curl_handle = curl_init($allocine_rss);
+        curl_setopt($rss_curl_handle, CURLOPT_RETURNTRANSFER, true); //curlopt_returntransfer retourne le fichier sous forme de chaînes de caractères
+        $contenu = curl_exec($rss_curl_handle) or die(curl_error($rss_curl_handle));
         if(!empty($contenu))
         {
             file_put_contents($cache_allocine, $contenu);
@@ -23,7 +24,7 @@
     }
     else
     {
-        $contenu = file_get_contents($cache_allocine);
+        $contenu ??= file_get_contents($cache_allocine);
     }
 
     
