2d9188c873a2d03dcf99f43ee109710e4df20298
Morgan-Saturn <morgan.tunteva@proton.me>
2026-07-22 10:00:33 +0200

    moved the cURL method to a new file and index.php is now getting it with a require once

--- index.php
+++ index.php
@@ -5,36 +5,8 @@
     
     libxml_use_internal_errors(true); //permet de gérer les erreurs sans crasher tout le script php
 
-    $contenu = NULL;
-    $allocine_rss = "https://www.allocine.fr/rss/news-cine.xml";
-    $cache_allocine = "cache/cache_allocine.xml";
-    
-        if (!file_exists($cache_allocine) || time() - filemtime($cache_allocine) > 1800) //1800s = 30 min
-        {
-            try
-            {
-            //cURL sert à faire des appels d'API, fetch des pages web ou envoyer des données à des serveurs avec des url. Il permet de faire des requêtes http.
-            $rss_curl_handle = curl_init($allocine_rss);
-            curl_setopt($rss_curl_handle, CURLOPT_RETURNTRANSFER, true); //curlopt_returntransfer retourne le fichier sous forme de chaînes de caractères
-            $contenu = curl_exec($rss_curl_handle) or die(curl_error($rss_curl_handle));
-            }
-
-            catch (Exception $e)
-            {
-                echo("Couldn't get the rss feed");
-            }
-
-            if(!empty($contenu))
-            {
-                file_put_contents($cache_allocine, $contenu);
-            }
-            else
-            {
-                $contenu = file_get_contents($cache_allocine);
-            }
-            
-        }
-    $contenu ??= file_get_contents($cache_allocine);
+    //getting the xml file inside of $contenu
+    require_once 'loading_xml_file.php';
 
     $xml = new SimpleXMLElement($contenu);
     $html = "";
@@ -97,5 +69,6 @@
                     } ?>
         </div>
     </div>
+    <script src="index.js"></script>
 </body>
 </html>
