d8b2956184005da545b062e51189590cb24642e2
Morgan-Saturn <morgan.tunteva@proton.me>
2026-07-23 11:23:42 +0200

    all the news are now displayed using a for of once the fetch method gives us its response. (also deleted useless comments in index.html)

--- fetch.js
+++ fetch.js
@@ -1,3 +1,6 @@
+const parentElement = document.querySelector(".movie_grid");
+const loadMore = document.querySelector('.voir_plus');
+
 //fetching the json php gives us
 async function fetchData() {
 
@@ -9,7 +12,19 @@ async function fetchData() {
         }
 
         const data = await response.json();
-        console.log(data);
+        let html = '';
+        for (const item of data) {
+            html += `
+                <div class='movie'>
+                    <img class='movie_img' src='${item.img}' style='width: 100%; height: 100%;' alt='affiche du film'>
+                    <h3 class='categories'>
+                        <a href='${item.link}'>${item.title}</a>
+                    </h3>
+                    <p class='summary'>${item.description}</p>
+                </div>
+                `;
+        }
+        parentElement.innerHTML = html;
         
     }
     catch(error){
