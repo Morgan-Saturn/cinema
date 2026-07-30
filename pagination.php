57bfbea23da6b3558dfadb8ef313049c552a7b81
Morgan-Saturn <morgan.tunteva@proton.me>
2026-07-30 13:51:32 +0200

    fixed a max to the numer of data the user can ask, using min() so no matter what the user enters, it won't return him or try to treat more than it actually can and have.

--- pagination.php
+++ pagination.php
@@ -4,11 +4,18 @@ require 'loading_xml_file.php';
 
     //pagination
     function pagination($data_array) {
-        $item_per_page = isset($_GET['perPage']) ? intval($_GET['perPage']) : 6;
-        $current_page = isset($_GET['page']) ? intval($_GET['page']) : 0;
+        
+        
         $total_news = count($data_array);
+        $item_per_page = min(isset($_GET['perPage']) ? intval($_GET['perPage']) : 6, $total_news);
+        $total_pages = ceil($total_news/$item_per_page);
+        $current_page = min(isset($_GET['page']) ? intval($_GET['page']) : 0, $total_pages);
         $begin = $current_page * $item_per_page;
         $paginated_news = array_slice($data_array,$begin, $item_per_page);
+
+        if ($current_page > $total_pages) {
+            $current_page = $total_pages;
+        }
 
         return [
             'paginated_news' => $paginated_news,
